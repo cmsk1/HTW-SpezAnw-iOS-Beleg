7671137f64762bd3792da21c5c3e1222bbdccd98
cs070 <constantin.schulte-kersmecke@rbb-online.de>
2021-10-26 16:16:05 +0200

    Initial Commit

---
+++ HTW-SpezAnw-Beleg/HTW_SpezAnw_BelegApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTW_SpezAnw_BelegApp.swift
+//  HTW-SpezAnw-Beleg
+//
+//  Created by Constantin Schulte-Kersmecke on 26.10.21.
+//
+
+import SwiftUI
+
+@main
+struct HTW_SpezAnw_BelegApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
